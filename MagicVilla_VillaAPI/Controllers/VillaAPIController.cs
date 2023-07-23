31165ad950101ff7412f95981502ba98d71d1681
praveenuk7 <praveenuk9@gmail.com>
2023-07-23 23:20:26 +0530

    Custom Logger

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Logging;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
@@ -10,11 +11,19 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
+        private readonly ILogging _logger;
+        public VillaAPIController(ILogging logger )
+        {
+            _logger = logger;
+        }
+
         //GET
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
+            _logger.Log("Getting All Villas","");
+
             return Ok(VillaStore.villaList);
         }
 
@@ -27,6 +36,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if(id == 0)
             {
+                _logger.Log("Get Villa error with id "+id,"error");
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
